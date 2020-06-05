8ecf66527d68ff8184f3325e92a36ef7bccb98ed
Your Name <you@example.com>
2020-06-05 15:58:47 +0530

    Added POST, PUT, PATCH and DELETE functions to our APIView.

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser   #Base for our user model
-from django.contrib.auth.models import PermissionsMixin
-from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import PermissionsMixin    #Controls permisiions granted to user
+from django.contrib.auth.models import BaseUserManager    
 # Create your models here.
 
 class UserProfileManager(BaseUserManager):
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView    #This imports the APIView class from the django REST Framework.
 from rest_framework.response import Response   #Standard response object that we return from our APIView and that can be rendered into an API output.
+from rest_framework import status    #It contains HTTP standard codes like HTTP 404,etc.
+
+from . import serializers
 
 # Create your views here.
 
 class HelloApiView(APIView):
     """Test API View."""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Return a list of APIView features."""
@@ -18,4 +22,33 @@ class HelloApiView(APIView):
             'Is mapped manually to URLs.'
         ]
 
-        return Response({'message' : 'Hello!', 'an_apiview' : an_apiview})
+        return Response({'message' : "Hello!", 'an_apiview' : an_apiview})
+
+
+    def post(self, request):
+        """Creates a hello message with our name."""
+
+        serializer = serializers.HelloSerializer(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {}'.format(name)
+            return Response({'Message':message})
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handles updating an object"""
+
+        return Response({'method' : 'put'})
+
+    def patch(self, request, pk=None):
+        """Patch request, only updates the fields provided in the request."""
+
+        return Response({'method' : 'patch'})
+
+    def delete(self, request, pk=None):
+        """Deletes an object."""
+
+        return Response({'method' : 'delete'})
