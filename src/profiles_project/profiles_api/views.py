8e766cc7bacdf922eb762d3f8c44eb802267ab39
Your Name <you@example.com>
2020-06-05 19:18:33 +0530

    Added create, retrieve, update, partial update, destroy functions to our viewset.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -55,8 +55,10 @@ class HelloApiView(APIView):
 
         return Response({'method' : 'delete'})
 
+
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
 
     def list(self, request):    #This is same as GET function as in APIView.
         """Retruns a hello message."""
@@ -68,3 +70,39 @@ class HelloViewSet(viewsets.ViewSet):
         ]
 
         return Response({'message': 'Hello!', 'a_viewset' : a_viewset})
+
+
+    def create(self, request):
+        """Creates a hello message with our name."""
+
+        serializer = serializers.HelloSerializer(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {}'.format(name)
+            return Response({'Message':message})
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def retrieve(self, request, pk=None):
+        """Handles getting an object bt it's ID."""
+
+        return Response({'http_method' : 'GET'})
+
+    def update(self, request, pk=None):
+        """Handles updating an object"""
+
+        return Response({'http_method' : 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handles updating part of an object."""
+
+        return Response({'http_method' : 'PATCH'})
+
+
+    def destroy(self, request, pk=None):
+        """Handles removing an object."""
+
+        return Response({'http_method' : 'DELETE'})
