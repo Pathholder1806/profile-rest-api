66d0a80b05cff4c9cb8f813608456271d442dfcd
Your Name <you@example.com>
2020-06-14 10:00:18 +0530

    Added login API.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -7,6 +7,8 @@ from rest_framework.response import Response   #Standard response object that we
 from rest_framework import status    #It contains HTTP standard codes like HTTP 404,etc.
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from . import serializers
 from . import models
@@ -113,8 +115,7 @@ class HelloViewSet(viewsets.ViewSet):
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-    """Handles creating and updating profile."""
-
+    """Handle creating and updating profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
 
@@ -123,3 +124,15 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class LoginViewSet(viewsets.ViewSet):
+    """Checks email and password and returns a auth token."""
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+
+        """Use the ObtainAuthToken APIView to validate and create a token."""
+
+        return ObtainAuthToken().post(request)
